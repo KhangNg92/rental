b73429ad3c0ebcd055fbe603baefd8d0c4aa8968
Khang Nguyen <mkavo92@gmail.com>
2019-02-16 12:52:16 -0600

    deploying

--- server/config/dev.js
+++ server/config/dev.js
@@ -1,7 +1,7 @@
 module.exports = {
     DB_URL: "mongodb://hackathon-project:hackathon1@ds237955.mlab.com:37955/hackathon-project",
     SECRET: "khang",
-    AWS_ACCESS_KEY_ID: "AKIAIQCBH2YNWMGHEL5A",
-    AWS_SECRET_ACCESS_KEY: "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",
+    AWS_ACCESS_KEY_ID: "AKIAI5B4YH7Q2ZUBSWIA",
+    AWS_SECRET_ACCESS_KEY: "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",
     STRIPE_SECRET_KEY: 'sk_test_HJZCXFE16pWFo9FnVPT6IDa3'
 };
