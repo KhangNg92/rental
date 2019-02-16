04eefb18c3cc93a558a155af4ad7eb88c3d0ed71
Khang Nguyen <mkavo92@gmail.com>
2019-02-16 10:12:53 -0600

    deploying

--- client/src/app/services/rental.service.ts
+++ client/src/app/services/rental.service.ts
@@ -4,48 +4,48 @@ import { Rental } from "../models/rental.model";
 import { HttpClient } from "@angular/common/http";
 
 @Injectable({
-  providedIn: "root"
+    providedIn: "root"
 })
 export class RentalService {
-  private readonly rootURL = "https://e-rental.herokuapp.com/api/rentals";
-  constructor(private httpClient: HttpClient) { }
+    private readonly rootURL = "https://e-rental.herokuapp.com/api/rentals";
+    constructor(private httpClient: HttpClient) { }
 
-  public getRentalById(rentalId: string): Observable<any> {
-    return this.httpClient.get(this.rootURL + "/" + rentalId);
-  }
+    public getRentalById(rentalId: string): Observable<any> {
+        return this.httpClient.get(this.rootURL + "/" + rentalId);
+    }
 
-  private getHeaders() {
-    return { "content-type": "application-json" };
-  }
-  public getRentals(): Observable<any> {
-    // we can only use arrow function here because it will bind the outer context
-    // to this context. we CAN'T use the function method here because it won't do the same way
-    return this.httpClient.get(this.rootURL);
-  }
+    private getHeaders() {
+        return { "content-type": "application-json" };
+    }
+    public getRentals(): Observable<any> {
+        // we can only use arrow function here because it will bind the outer context
+        // to this context. we CAN'T use the function method here because it won't do the same way
+        return this.httpClient.get(this.rootURL);
+    }
 
-  public getRentalsByCity(city: string): Observable<any> {
-    // we can only use arrow function here because it will bind the outer context
-    // to this context. we CAN'T use the function method here because it won't do the same way
-    return this.httpClient.get(this.rootURL + `?city=${city}`);
-  }
+    public getRentalsByCity(city: string): Observable<any> {
+        // we can only use arrow function here because it will bind the outer context
+        // to this context. we CAN'T use the function method here because it won't do the same way
+        return this.httpClient.get(this.rootURL + `?city=${city}`);
+    }
 
-  public createRental(rental: Rental): Observable<any> {
-    return this.httpClient.post(this.rootURL, rental);
-  }
+    public createRental(rental: Rental): Observable<any> {
+        return this.httpClient.post(this.rootURL, rental);
+    }
 
-  public getUserRentals(): Observable<any> {
-    return this.httpClient.get(this.rootURL + "/manage");
-  }
+    public getUserRentals(): Observable<any> {
+        return this.httpClient.get(this.rootURL + "/manage");
+    }
 
-  public deleteRental(rentalId: string): Observable<any> {
-    return this.httpClient.delete(this.rootURL + "/" + rentalId);
-  }
+    public deleteRental(rentalId: string): Observable<any> {
+        return this.httpClient.delete(this.rootURL + "/" + rentalId);
+    }
 
-  public updateRental(rentalId: string, rentalData: any) {
-    return this.httpClient.patch(this.rootURL + "/" + rentalId, rentalData);
-  }
+    public updateRental(rentalId: string, rentalData: any) {
+        return this.httpClient.patch(this.rootURL + "/" + rentalId, rentalData);
+    }
 
-  public verifyRentalUser(rentalId: string): Observable<any> {
-    return this.httpClient.get(this.rootURL + `/${rentalId}/verify-user`);
-  }
+    public verifyRentalUser(rentalId: string): Observable<any> {
+        return this.httpClient.get(this.rootURL + `/${rentalId}/verify-user`);
+    }
 }
