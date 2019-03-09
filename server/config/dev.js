b49f14bafe90192dea2bcf2dc0bd2adaa226bf0f
Khang Nguyen <mkavo92@gmail.com>
2019-03-09 11:49:44 -0600

    ignoring

--- server/config/dev.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    DB_URL: "mongodb://rental:minhkhang1@ds343895.mlab.com:43895/rental",
-    SECRET: "khang",
-    AWS_ACCESS_KEY_ID: "AKIAJ2UMWENBKORLLGVQ",
-    AWS_SECRET_ACCESS_KEY: "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",
-    STRIPE_SECRET_KEY: 'sk_test_HJZCXFE16pWFo9FnVPT6IDa3'
-};
